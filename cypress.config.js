ef04e7d34ea037f0518cc91151583d1e53b1f715
shahlamammadova99 <shahla.mammadova.ext@qantev.com>
2025-05-28 16:56:11 +0400

    add reportter generator

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,15 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
+    e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-
-
-      
-    },
+    }, 
   },
-});
+  reporter: 'mochawesome',
+  reporterOptions: {
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+})
